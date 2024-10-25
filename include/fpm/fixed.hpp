5c9991211f2e83eb66254d1245886f0158312147
Abit Gray <AbitTheGray@gmail.com>
2024-10-26 01:58:45 +0200

    Add 128-bit integers

--- include/fpm/fixed.hpp
+++ include/fpm/fixed.hpp
@@ -8,6 +8,10 @@
 #include <limits>
 #include <type_traits>
 
+#if defined(WIN32) && defined(_MSC_VER)
+#include <__MSVC_Int128.hpp>
+#endif
+
 namespace fpm
 {
 
@@ -206,9 +210,33 @@ private:
 // Convenience typedefs
 //
 
-using fixed_16_16 = fixed<std::int32_t, std::int64_t, 16>;
+using fixed_8_8 = fixed<std::int16_t, std::int32_t, 8>;
+
 using fixed_24_8 = fixed<std::int32_t, std::int64_t, 8>;
+using fixed_16_16 = fixed<std::int32_t, std::int64_t, 16>;
 using fixed_8_24 = fixed<std::int32_t, std::int64_t, 24>;
+
+#if defined(FPM_INT128)
+// Already defined
+#elif defined(__SIZEOF_INT128__)
+using int128_t = __int128_t;
+#define FPM_INT128 ::fpm::int128_t
+#elif defined(WIN32) && defined(_MSC_VER)
+using int128_t = std::_Signed128;
+#define FPM_INT128 ::fpm::int128_t
+#else
+#warning 128-bit numbers not supported.
+#endif
+
+#ifdef FPM_INT128
+static_assert(sizeof(FPM_INT128) > sizeof(std::int64_t));
+static_assert(std::is_signed<FPM_INT128>::value);
+using fixed_56_8  = fixed<std::int64_t, FPM_INT128, 8>;
+using fixed_48_16 = fixed<std::int64_t, FPM_INT128, 16>;
+using fixed_32_32 = fixed<std::int64_t, FPM_INT128, 32>;
+using fixed_16_48 = fixed<std::int64_t, FPM_INT128, 48>;
+using fixed_8_56  = fixed<std::int64_t, FPM_INT128, 56>;
+#endif
 
 //
 // Addition
